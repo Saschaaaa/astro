770d2530a40a249ce6abf487cb6e2dc19f79ee17
Sascha Folk <fosa1012@hs-karlsruhe.de>
2023-04-05 15:54:57 +0200

    readme update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 import node from "@astrojs/node";
@@ -8,9 +9,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind()]
 });
